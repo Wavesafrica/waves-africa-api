7cb57396f98cdd78b45e071e2812f4b9fce0ffef
Kelvin Omereshone <kelvinomereshone@gmail.com>
2019-05-14 00:15:47 +0100

    feat(index.js): implemented endpoint to return back waves equivalent in naira

---
+++ api/controllers/index.js
@@ -0,0 +1,15 @@
+/**
+ * Module dependencies
+ */
+
+// ...
+
+
+/**
+ * index.js
+ *
+ * Index something.
+ */
+module.exports = async function index(req, res) {
+  return res.status(200).json({message: 'Welcome to Waves Africa Web Service'});
+};
