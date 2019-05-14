00c3a3decb9ada9ec8037fad61a9cc65c901aa75
Kelvin Omereshone <kelvinomereshone@gmail.com>
2019-05-14 07:50:14 +0100

    chore: set up proxy

--- config/env/production.js
+++ config/env/production.js
@@ -322,7 +322,7 @@ module.exports = {
     * (https://sailsjs.com/config/http)                                        *
     *                                                                          *
     ***************************************************************************/
-    // trustProxy: true,
+    trustProxy: true,
 
   },
 
