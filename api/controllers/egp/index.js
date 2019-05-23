e0697c85b8d37fea4405388b7fe47e94695d4ced
Kelvin Omereshone <kelvinomereshone@gmail.com>
2019-05-23 16:45:12 +0100

    feat(egp/index): Added support for Egyptian pound

---
+++ api/controllers/egp/index.js
@@ -0,0 +1,32 @@
+/**
+ * Module dependencies
+ */
+
+const axios = require('axios');
+
+// ...
+
+
+/**
+ * egp/index.js
+ *
+ * Index egp.
+ */
+module.exports = async function index(req, res) {
+  const config = {
+    headers: {'Authorization': 'Apikey ee05c52992071556a818f4f0b2c08d21f9cc815afb499e65bbfd34a220ae0cd7'}
+  };
+  axios.all([
+    axios.get('https://min-api.cryptocompare.com/data/price?fsym=WAVES&tsyms=USD', config),
+    axios.get('https://free.currconv.com/api/v7/convert?q=USD_EGP&compact=ultra&apiKey=3ea8e1b98e3f3fb48b2e')
+  ]).then(axios.spread((waves, currency) => {
+    const price = waves.data.USD * currency.data.USD_EGP;
+    res.status(200).json({WAVES_EGP: price.toFixed(2)});
+  }))
+  .catch(error => {
+    sails.log(error);
+    res.status(500).json({message: "Something went wrong and it's not your fault"});
+  });
+
+
+};
