38e7bf3bfb831a499677cc9036eb082c643a94db
Kelvin Omereshone <kelvinomereshone@gmail.com>
2019-05-23 15:29:06 +0100

    --no-edits

---
+++ api/controllers/ugx/index.js
@@ -0,0 +1,29 @@
+/**
+ * Module dependencies
+ */
+const axios = require('axios');
+// ...
+
+
+/**
+ * ugx/index.js
+ *
+ * Index ugx.
+ */
+module.exports = async function index(req, res) {
+
+  const config = {
+    headers: {'Authorization': 'Apikey ee05c52992071556a818f4f0b2c08d21f9cc815afb499e65bbfd34a220ae0cd7'}
+  };
+  axios.all([
+    axios.get('https://min-api.cryptocompare.com/data/price?fsym=WAVES&tsyms=USD', config),
+    axios.get('https://free.currconv.com/api/v7/convert?q=USD_UGX&compact=ultra&apiKey=3ea8e1b98e3f3fb48b2e')
+  ]).then(axios.spread((waves, currency) => {
+    const price = waves.data.USD * currency.data.USD_UGX;
+    res.status(200).json({WAVES_UGX: price.toFixed(2)});
+  }))
+  .catch(error => {
+    sails.log(error);
+    res.status(500).json({message: "Something went wrong and it's not your fault"});
+  });
+};
