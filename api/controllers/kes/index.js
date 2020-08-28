c4077dc2986a0fda17844e3eec243b0f83347cc8
Achilles45 <achillesusuoyibo7@gmail.com>
2020-08-28 11:45:13 -0700

    feat: converted endpoints to action2

--- api/controllers/kes/index.js
+++ api/controllers/kes/index.js
@@ -1,38 +1,40 @@
-/**
- * Module dependencies
- */
-const axios = require('axios');
-// ...
+module.exports = {
+  friendlyName: "Index",
 
+  description: "Index lyd.",
 
-/**
- * kes/index.js
- *
- * Index kes.
- */
-module.exports = async function index(req, res) {
+  inputs: {
+    amount: {
+      type: "number",
+    },
+  },
 
-  const config = {
-    headers: {'Authorization': 'Apikey ee05c52992071556a818f4f0b2c08d21f9cc815afb499e65bbfd34a220ae0cd7'}
-  };
-  axios.all([
-    axios.get('https://min-api.cryptocompare.com/data/price?fsym=WAVES&tsyms=USD', config),
-    axios.get('https://free.currconv.com/api/v7/convert?q=USD_KES&compact=ultra&apiKey=3ea8e1b98e3f3fb48b2e')
-  ]).then(axios.spread((waves, currency) => {
-    var price = waves.data.USD * currency.data.USD_KES;
+  exits: {
+    operationalError: {
+      statusCode: 400,
+    },
+  },
 
-    const data = req.allParams();
+  fn: async function (inputs) {
+    try {
+      const price = await sails.helpers.getWavesPriceInCurrency.with({
+        amount: inputs.amount,
+        currencyCode: "LYD",
+      });
 
-    var amount = data.amount || 1;
-
-    if (!_.isUndefined(amount) && !isNaN(Number(amount))) {
-      price = price * parseFloat(amount);
+      exits.success({ WAVES_LYD: price });
+    } catch (error) {
+      sails.log(error);
+      if (error.isOperationalError) {
+        return exits.operationalError({
+          message: "Something went wrong",
+          error: error.raw,
+        });
+      }
+      return exits.error({
+        message: "Something went wrong",
+        error: error.message,
+      });
     }
-
-    res.status(200).json({WAVES_KES: price.toFixed(2)});
-  }))
-  .catch(error => {
-    sails.log(error);
-    res.status(500).json({message: "Something went wrong and it's not your fault"});
-  });
+  },
 };
